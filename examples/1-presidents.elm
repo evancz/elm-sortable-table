1a5a1b914e17a0b6b7b7d31f6b23fa03c8927044
Bill St. Clair <billstclair@gmail.com>
2018-08-28 15:24:26 -0400

    elm-upgrade to Elm 0.19.
    Fix shadowing and toString problems.
    
    Change now nonexistent  functions to floating point math.
    
    Add a CSS file for the example.
    
    Update live sample links to billstclair.github.io.

--- examples/1-presidents.elm
+++ examples/1-presidents.elm
@@ -1,17 +1,19 @@
+module Main exposing (Model, Msg(..), Person, config, init, main, presidents, update, view)
+
+import Browser
 import Html exposing (Html, div, h1, input, text)
 import Html.Attributes exposing (placeholder)
 import Html.Events exposing (onInput)
 import Table
 
 
-
 main =
-  Html.program
-    { init = init presidents
-    , update = update
-    , view = view
-    , subscriptions = \_ -> Sub.none
-    }
+    Browser.element
+        { init = \() -> init presidents
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
 
 
 
@@ -19,21 +21,21 @@ main =
 
 
 type alias Model =
-  { people : List Person
-  , tableState : Table.State
-  , query : String
-  }
+    { people : List Person
+    , tableState : Table.State
+    , query : String
+    }
 
 
 init : List Person -> ( Model, Cmd Msg )
 init people =
-  let
-    model =
-      { people = people
-      , tableState = Table.initialSort "Year"
-      , query = ""
-      }
-  in
+    let
+        model =
+            { people = people
+            , tableState = Table.initialSort "Year"
+            , query = ""
+            }
+    in
     ( model, Cmd.none )
 
 
@@ -42,22 +44,22 @@ init people =
 
 
 type Msg
-  = SetQuery String
-  | SetTableState Table.State
+    = SetQuery String
+    | SetTableState Table.State
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    SetQuery newQuery ->
-      ( { model | query = newQuery }
-      , Cmd.none
-      )
+    case msg of
+        SetQuery newQuery ->
+            ( { model | query = newQuery }
+            , Cmd.none
+            )
 
-    SetTableState newState ->
-      ( { model | tableState = newState }
-      , Cmd.none
-      )
+        SetTableState newState ->
+            ( { model | tableState = newState }
+            , Cmd.none
+            )
 
 
 
@@ -66,32 +68,32 @@ update msg model =
 
 view : Model -> Html Msg
 view { people, tableState, query } =
-  let
-    lowerQuery =
-      String.toLower query
+    let
+        lowerQuery =
+            String.toLower query
 
-    acceptablePeople =
-      List.filter (String.contains lowerQuery << String.toLower << .name) people
-  in
+        acceptablePeople =
+            List.filter (String.contains lowerQuery << String.toLower << .name) people
+    in
     div []
-      [ h1 [] [ text "Birthplaces of U.S. Presidents" ]
-      , input [ placeholder "Search by Name", onInput SetQuery ] []
-      , Table.view config tableState acceptablePeople
-      ]
+        [ h1 [] [ text "Birthplaces of U.S. Presidents" ]
+        , input [ placeholder "Search by Name", onInput SetQuery ] []
+        , Table.view config tableState acceptablePeople
+        ]
 
 
 config : Table.Config Person Msg
 config =
-  Table.config
-    { toId = .name
-    , toMsg = SetTableState
-    , columns =
-        [ Table.stringColumn "Name" .name
-        , Table.intColumn "Year" .year
-        , Table.stringColumn "City" .city
-        , Table.stringColumn "State" .state
-        ]
-    }
+    Table.config
+        { toId = .name
+        , toMsg = SetTableState
+        , columns =
+            [ Table.stringColumn "Name" .name
+            , Table.intColumn "Year" .year
+            , Table.stringColumn "City" .city
+            , Table.stringColumn "State" .state
+            ]
+        }
 
 
 
@@ -99,57 +101,57 @@ config =
 
 
 type alias Person =
-  { name : String
-  , year : Int
-  , city : String
-  , state : String
-  }
+    { name : String
+    , year : Int
+    , city : String
+    , state : String
+    }
 
 
 presidents : List Person
 presidents =
-  [ Person "George Washington" 1732 "Westmoreland County" "Virginia"
-  , Person "John Adams" 1735 "Braintree" "Massachusetts"
-  , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
-  , Person "James Madison" 1751 "Port Conway" "Virginia"
-  , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
-  , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
-  , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
-  , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
-  , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
-  , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
-  , Person "John Tyler" 1790 "Charles City County" "Virginia"
-  , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
-  , Person "James K. Polk" 1795 "Pineville" "North Carolina"
-  , Person "Millard Fillmore" 1800 "Summerhill" "New York"
-  , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
-  , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
-  , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
-  , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
-  , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
-  , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
-  , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
-  , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
-  , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
-  , Person "William McKinley" 1843 "Niles" "Ohio"
-  , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
-  , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
-  , Person "Theodore Roosevelt" 1858 "New York City" "New York"
-  , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
-  , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
-  , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
-  , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
-  , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
-  , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
-  , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
-  , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
-  , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
-  , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
-  , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
-  , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
-  , Person "Jimmy Carter" 1924 "Plains" "Georgia"
-  , Person "George W. Bush" 1946 "New Haven" "Connecticut"
-  , Person "Bill Clinton" 1946 "Hope" "Arkansas"
-  , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
-  , Person "Donald Trump" 1946 "New York City" "New York"
-  ]
+    [ Person "George Washington" 1732 "Westmoreland County" "Virginia"
+    , Person "John Adams" 1735 "Braintree" "Massachusetts"
+    , Person "Thomas Jefferson" 1743 "Shadwell" "Virginia"
+    , Person "James Madison" 1751 "Port Conway" "Virginia"
+    , Person "James Monroe" 1758 "Monroe Hall" "Virginia"
+    , Person "Andrew Jackson" 1767 "Waxhaws Region" "South/North Carolina"
+    , Person "John Quincy Adams" 1767 "Braintree" "Massachusetts"
+    , Person "William Henry Harrison" 1773 "Charles City County" "Virginia"
+    , Person "Martin Van Buren" 1782 "Kinderhook" "New York"
+    , Person "Zachary Taylor" 1784 "Barboursville" "Virginia"
+    , Person "John Tyler" 1790 "Charles City County" "Virginia"
+    , Person "James Buchanan" 1791 "Cove Gap" "Pennsylvania"
+    , Person "James K. Polk" 1795 "Pineville" "North Carolina"
+    , Person "Millard Fillmore" 1800 "Summerhill" "New York"
+    , Person "Franklin Pierce" 1804 "Hillsborough" "New Hampshire"
+    , Person "Andrew Johnson" 1808 "Raleigh" "North Carolina"
+    , Person "Abraham Lincoln" 1809 "Sinking spring" "Kentucky"
+    , Person "Ulysses S. Grant" 1822 "Point Pleasant" "Ohio"
+    , Person "Rutherford B. Hayes" 1822 "Delaware" "Ohio"
+    , Person "Chester A. Arthur" 1829 "Fairfield" "Vermont"
+    , Person "James A. Garfield" 1831 "Moreland Hills" "Ohio"
+    , Person "Benjamin Harrison" 1833 "North Bend" "Ohio"
+    , Person "Grover Cleveland" 1837 "Caldwell" "New Jersey"
+    , Person "William McKinley" 1843 "Niles" "Ohio"
+    , Person "Woodrow Wilson" 1856 "Staunton" "Virginia"
+    , Person "William Howard Taft" 1857 "Cincinnati" "Ohio"
+    , Person "Theodore Roosevelt" 1858 "New York City" "New York"
+    , Person "Warren G. Harding" 1865 "Blooming Grove" "Ohio"
+    , Person "Calvin Coolidge" 1872 "Plymouth" "Vermont"
+    , Person "Herbert Hoover" 1874 "West Branch" "Iowa"
+    , Person "Franklin D. Roosevelt" 1882 "Hyde Park" "New York"
+    , Person "Harry S. Truman" 1884 "Lamar" "Missouri"
+    , Person "Dwight D. Eisenhower" 1890 "Denison" "Texas"
+    , Person "Lyndon B. Johnson" 1908 "Stonewall" "Texas"
+    , Person "Ronald Reagan" 1911 "Tampico" "Illinois"
+    , Person "Richard M. Nixon" 1913 "Yorba Linda" "California"
+    , Person "Gerald R. Ford" 1913 "Omaha" "Nebraska"
+    , Person "John F. Kennedy" 1917 "Brookline" "Massachusetts"
+    , Person "George H. W. Bush" 1924 "Milton" "Massachusetts"
+    , Person "Jimmy Carter" 1924 "Plains" "Georgia"
+    , Person "George W. Bush" 1946 "New Haven" "Connecticut"
+    , Person "Bill Clinton" 1946 "Hope" "Arkansas"
+    , Person "Barack Obama" 1961 "Honolulu" "Hawaii"
+    , Person "Donald Trump" 1946 "New York City" "New York"
+    ]
--- examples/README.md
+++ examples/README.md
@@ -1,7 +1,7 @@
 # Examples
 
-  1. [U.S. Presidents by Birth Place](https://evancz.github.io/elm-sortable-table/presidents.html)
-  2. [Travel Planner for the Mission District in San Francisco](https://evancz.github.io/elm-sortable-table/travel.html)
+  1. [U.S. Presidents by Birth Place](https://billstclair.github.io/elm-sortable-table/presidents.html)
+  2. [Travel Planner for the Mission District in San Francisco](https://billstclair.github.io/elm-sortable-table/travel.html)
 
 
 ## Build Instructions
@@ -9,7 +9,7 @@
 To see the examples *without* CSS, run the following commands:
 
 ```bash
-git clone https://github.com/evancz/elm-sortable-table.git
+git clone https://github.com/billstclair/elm-sortable-table.git
 cd elm-sortable-table
 cd examples
 elm-reactor
@@ -23,17 +23,21 @@ Then navigate to `1-presidents.elm` or `2-travel.elm` from [localhost:8000](http
 To see the examples *with* CSS, run the following commands:
 
 ```bash
-git clone https://github.com/evancz/elm-sortable-table.git
+git clone https://github.com/billstclair/elm-sortable-table.git
 cd elm-sortable-table
 cd examples
-elm-make 1-presidents.elm --yes --output=elm.js
+elm make 1-presidents.elm --output=site/presidents.js
 elm-reactor
 ```
 
-Then open [localhost:8000/index.html](http://localhost:8000/index.html) in your browser. That HTML file loads in some CSS and whatever code is in `elm.js`. So if you want to see the second example with CSS, you can compile it like this:
+Then open [localhost:8000/site/presidents.html](http://localhost:8000/site/presidents.html) in your browser. That HTML file loads in some CSS and whatever code is in `presidents.js`.
+
+If you want to see the second example with CSS, you can compile it like this:
 
 ```bash
-elm-make 2-travel.elm --yes --output=elm.js
+elm make 2-travel.elm --output=site/travel.js
 ```
+
+And then open [localhost:8000/site/travel.html](http://localhost:8000/site/travel.html).
 
 As you make changes, you will want to recompile the Elm code with `elm-make`.
